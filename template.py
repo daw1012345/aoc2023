cd7cd75b218d665bc09fe6f7a71dec07fd87b90e
Dawid Kulikowski <git@dawidkulikowski.pl>
2023-11-29 09:55:34 +0100

    Create a template file

---
+++ template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+INPUT = "input.txt"
+
+
+def part1(data):
+    return data
+
+
+def part2(data):
+    return data
+
+
+def parse():
+    with open(INPUT, "r") as fd:
+        return fd.read()
+
+
+if __name__ == "__main__":
+    d = parse()
+    part1(d)
+    part2(d)
