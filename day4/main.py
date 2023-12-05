e5d105948907aa679ffb99cbdd952950c797296a
Dawid Kulikowski <git@dawidkulikowski.pl>
2023-12-05 11:43:15 +0100

    Reformat day 4

--- day4/main.py
+++ day4/main.py
@@ -18,6 +18,7 @@ def part1(data):
         total += partial_total
     return total
 
+
 def part2(data):
     data = [(i, x[0], x[1]) for i, x in enumerate(data)]
     queue = data[:]
@@ -31,24 +32,28 @@ def part2(data):
         for n in nums:
             if n in winning:
                 matches += 1
-        for x in range(1, matches+1):
+        for x in range(1, matches + 1):
             # card_num = data.index(card)
             # print(f"Won {card_num+x+1}")
-            queue.append(data[card_num+x])
-            
+            queue.append(data[card_num + x])
+
         total += matches
     return total
-
-
 
 
 def parse():
     with open(INPUT, "r") as fd:
         raw_data = fd.readlines()
 
-    raw_data = [x[x.index(':')+2:].strip() for x in raw_data]
-    raw_data = [x.split(' | ') for x in raw_data]
-    raw_data = [(list(filter(lambda a: a != '', x[0].split(' '))), list(filter(lambda a: a != '', x[1].split(' ')))) for x in raw_data]
+    raw_data = [x[x.index(":") + 2 :].strip() for x in raw_data]
+    raw_data = [x.split(" | ") for x in raw_data]
+    raw_data = [
+        (
+            list(filter(lambda a: a != "", x[0].split(" "))),
+            list(filter(lambda a: a != "", x[1].split(" "))),
+        )
+        for x in raw_data
+    ]
 
     return raw_data
 
